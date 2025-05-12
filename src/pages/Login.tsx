992cda4dfd0ee163695307c65cad02ab7bd700d5
sakshi Pawar <pawarsakshi089@gmail.com>
2025-05-12 14:52:07 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,7 +20,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8000/login', {
+      const response = await fetch('https://jobseeker-backen.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -105,3 +105,4 @@ const Login = () => {
 };
 
 export default Login;
+
